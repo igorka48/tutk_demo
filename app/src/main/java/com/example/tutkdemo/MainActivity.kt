6c41cb90a5bd0e1bb9ecd79a1dd9e4d760d0de27
Ihor Mishchenko <owls.developers@gmail.com>
2022-05-25 20:23:47 +0300

    added build time label

--- app/src/main/java/com/example/tutkdemo/MainActivity.kt
+++ app/src/main/java/com/example/tutkdemo/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         with(binding) {
+            buildTimeLabel.text = BuildConfig.BUILD_TIME
             scanUIDButton.setOnClickListener { requestCameraPermissionAndStartScanner() }
             exoPlayerButton.setOnClickListener { openExoPlayer() }
             ijkPlayerButton.setOnClickListener { openIjkPlayer() }
